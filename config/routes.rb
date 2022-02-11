2fb0e2b32c4320f47e2bbcbb2c9a8a05fc4bef42
Bao Vo <vodinhtbao@gmail.com>
2022-02-10 23:04:36 -0600

    for lab 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :user_books
+  resources :users
+  resources :books
+
+  root 'user_books#index'
+  
   #all of the resourcesful routes are already provided for books
   resources :books do
     member do
